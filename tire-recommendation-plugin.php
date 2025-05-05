0e7636455c11c116627b7b1639968243f17a35b0
A. A. Mugniul Haque <98876581+Mugniul30@users.noreply.github.com>
2025-05-06 02:51:08 +0600

    Change Author URI

--- tire-recommendation-plugin.php
+++ tire-recommendation-plugin.php
@@ -7,7 +7,7 @@
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Mugniul Afif
- * Author URI:        https://author.example.com/
+ * Author URI:        https://github.com/Mugniul30
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
